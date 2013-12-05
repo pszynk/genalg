4fbaf45bcc6a3e00c5cacc040b51efe5766c1b36
JT <ive@jt-ubuntu-e130.(none)>
2013-12-05 22:48:34 +0100

    funckja ackleya

---
+++ genalg/src/ackley.h
@@ -0,0 +1,19 @@
+#ifndef ACKLEY_H
+#define ACKLEY_H
+
+#include <math.h>
+
+#include "globals.h"
+#include "types.h"
+#include "myerr.h"
+
+#define ACKLEY_LB -40.0
+#define ACKLEY_UB  40.0
+#define ACKLEY_MAX_VAL(dim) (40.0 * (dim) + 1)
+
+real_t ackley_eval(const real_t *args, idx_t nargs);
+real_t ackley_reval(real_t eval);
+
+real_t ackley(const real_t *args, idx_t nargs);
+
+#endif /* end of include guard: ACKLEY_H */
